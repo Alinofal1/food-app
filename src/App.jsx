14e7589be1b07a834115cc8b111874911ea9a7ea
Alinofal1 <alinofal405@gmail.com>
2025-06-11 01:44:11 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Category from "./components/Category";
+import Food from "./components/Food";
+import HeadLineCards from "./components/HeadLineCards";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="px-20 md:px-28">
+      <Navbar />
+      <Hero />
+      <HeadLineCards />
+      <Food />
+      <Category />
+    </div>
+  );
+}
+
+export default App;
